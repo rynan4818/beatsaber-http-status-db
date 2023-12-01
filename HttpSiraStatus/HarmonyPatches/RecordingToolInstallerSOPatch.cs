f18d4d8a8a2bae1abd0d05cb47f9f6de18a285c6
denpadokei <jsoldenpa@yahoo.co.jp>
2023-12-01 22:59:58 +0900

    起動しないバグ

---
+++ HttpSiraStatus/HarmonyPatches/RecordingToolInstallerSOPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Zenject;
+
+namespace HttpSiraStatus.HarmonyPatches
+{
+    [HarmonyPatch(typeof(RecordingToolInstallerSO), nameof(RecordingToolInstallerSO.InstallDependencies))]
+    internal class RecordingToolInstallerSOPatch
+    {
+        [HarmonyPrefix]
+        public static void InstallDependenciesPrefix(RecordingToolInstallerSO __instance, DiContainer container, ProgramArguments programArguments, BeatmapCharacteristicCollection beatmapCharacteristicCollection, ref bool __runOriginal)
+        {
+            if (__runOriginal) {
+                var recordingToolManager = new RecordingToolManager(programArguments, __instance._recordingToolResourceContainer, beatmapCharacteristicCollection, container);
+                container.QueueForInject(recordingToolManager);
+                _ = container.BindInterfacesAndSelfTo<RecordingToolManager>().FromInstance(recordingToolManager).AsSingle();
+                _ = container.Bind<IBeatSaberLogger>().WithId("RecordingTool").FromInstance(recordingToolManager.logger).AsSingle();
+                _ = container.Bind<IPosesSerializer>().FromInstance(recordingToolManager.posesSerializer).AsSingle();
+                __runOriginal = false;
+            }
+        }
+    }
+}
